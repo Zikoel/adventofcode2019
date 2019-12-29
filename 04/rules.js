2fde47b0885c60f42a354578eb0884abb0403170
Elia Mazzuoli <zikoel@gmail.com>
2019-12-29 02:15:51 +0100

    Added a rule for the second step of exercise 04

--- 04/rules.js
+++ 04/rules.js
@@ -25,6 +25,34 @@ const adiacentCouples = str => {
 
 }
 
+const splitByAdiacentGroups = ( groups, digits ) => {
+  // console.log(groups)
+  // console.log(digits)
+  // console.log('--')
+
+  if ( _.isEmpty(digits) ) return groups
+
+  const digit = _.head(digits)
+
+  if(_.isEmpty(groups)) {
+    return splitByAdiacentGroups( [[digit]], _.tail(digits) )
+  }
+
+  const lastGroup = _.last(groups)
+  const lastGroupDigit = _.last(lastGroup)
+  if( lastGroupDigit === digit ) {
+    return splitByAdiacentGroups(
+      [ ..._.take(groups, groups.length - 1), [...lastGroup, digit] ],
+      _.tail(digits)
+    )
+  } else {
+    return splitByAdiacentGroups(
+      [ ...groups, [digit] ],
+      _.tail(digits)
+    )
+  }
+}
+
 const twoAdiacentDigits = psw => {
   if (psw.length < 2) return false
 
@@ -41,8 +69,21 @@ const neverDecreaseDigits = psw => {
   return _.every( couples, couple => couple[0] <= couple[1] )
 }
 
+const twoAdiacentButNotOnBiggerGroup = psw => {
+  if (psw.length < 2) return false
+  
+  const onlyTwoAdiacentElementsCount = splitByAdiacentGroups([], psw.split(""))
+    .map( group => group.length )
+    .filter( lenght => lenght === 2 )
+    .length
+
+  return onlyTwoAdiacentElementsCount > 0
+}
+
 module.exports = {
   length,
   twoAdiacentDigits,
-  neverDecreaseDigits
+  neverDecreaseDigits,
+  splitByAdiacentGroups,
+  twoAdiacentButNotOnBiggerGroup
 }
--- 04/test.spec.js
+++ 04/test.spec.js
@@ -16,6 +16,17 @@ test('psw should not pass the never decrease rule', () => {
   expect( rules.neverDecreaseDigits("122450") ).toBe(false)
 })
 
+test('psw should pass the twoAdiacentButNotOnBiggerGroup rule', () => {  
+  expect( rules.twoAdiacentButNotOnBiggerGroup("122456") ).toBe(true)
+  expect( rules.twoAdiacentButNotOnBiggerGroup("122446") ).toBe(true)
+  expect( rules.twoAdiacentButNotOnBiggerGroup("111122") ).toBe(true)
+})
+
+test('psw should not pass the twoAdiacentButNotOnBiggerGroup rule', () => {  
+  expect( rules.twoAdiacentButNotOnBiggerGroup("123444") ).toBe(false)
+  expect( rules.twoAdiacentButNotOnBiggerGroup("222444") ).toBe(false)
+})
+
 test('psw pass all the rules', () => {  
   const psw = 344567
 
